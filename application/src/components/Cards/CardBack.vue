96a44c09aa3585a76de987fc9e085c86e29551e7
Alan Nardo <alan.nardo.91@gmail.com>
2024-04-17 23:08:31 +0100

    made some SVGs for the flipside of the card. not sure to put anything there or leave blank since they look kinda cool. need to get flipping to be individual rather than all 6 at once. should probably jazz up the front-side too

--- application/src/components/Cards/CardBack.vue
+++ application/src/components/Cards/CardBack.vue
@@ -6,11 +6,44 @@ defineProps(['pokemon'])
 
 </script>
 <template>
-  <div class="card back card-body">
+  <div class="opacity-wrapper">
+    <div class="col card back align-items-center justify-content-evenly card-body">
 
-    <div class="h-100 w-100">Incomplete</div>
-
-
-    <!-- {{ pokemon.type }}</div> -->
+      <div class="card-body">
+        <div class="card-title">{{ pokemon.name[0].toUpperCase() + pokemon.name.slice(1) }}</div> 
+        <ul>
+          <li style="display: flex; flex-direction: row;" v-for="type in pokemon.type">{{ type }}</li>
+        </ul>
+        <!-- fill the rest of this in later, basically just a different version of the front, but i probably want to jazz it up a bit, otherwise it just makes sense to leave it blank -->
+        
+      </div>
+      
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+  .back {
+    background-image: url('/assets/cardback-bg2.svg');
+    background-size: cover;
+    background-position: center;
+    
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-basis: calc(100% / 6);
+    
+    background-color: rgba(255,255,255,.2);
+  } 
+
+
+  .opacity-wrapper {
+    padding: 0;
+    margin: 0;
+    height: 100%;
+    width: 100%;
+    background-color: whitesmoke;
+    border-radius: 10px;
+  }
+</style>
